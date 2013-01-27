2897ea1ae59af6ab24ca1b935b3fb99a1d32d617
Simon Wollwage <mail.wollwage@gmail.com>
2013-01-27 10:12:38 +0100

    fixed cmake file to output in same directory

--- code/src/RscClusterer.cpp
+++ code/src/RscClusterer.cpp
@@ -2,11 +2,8 @@
 
 void RscClusterer::cluster_soft_rsc(const std::string& temp_directory_path, const std::string& cluster_directory_path)
 {
-	if (this->initialize_soft_rsc())
-	{}
-
 	for (auto sample = -this->number_of_tiny_samples; sample < this->number_of_samples; ++sample)
 	{
-		
+
 	}
 }
--- code/src/RscClusterer.h
+++ code/src/RscClusterer.h
@@ -1,8 +1,13 @@
 #ifndef __RSC_CLUSTERER_H__
 #define __RSC_CLUSTERER_H__
 
+#include <string>
+
 class RscClusterer
 {
+private:
+	unsigned int number_of_tiny_samples;
+	unsigned int number_of_samples;
 public:
 	void cluster_soft_rsc(const std::string& temp_directory_path, const std::string& cluster_directory_path);
 };
