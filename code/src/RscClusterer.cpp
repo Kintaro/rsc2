623db46b8cfbba076f2cce6ba7b8bb040fb461f6
Simon Wollwage <mail.wollwage@gmail.com>
2013-02-06 05:08:11 +0100

    added stubs

--- code/src/RscClusterer.cpp
+++ code/src/RscClusterer.cpp
@@ -1,3 +1,43 @@
+// GreedyRSC (Greedy Relevant Set Correlation) Clustering Tool
+//
+// Copyright (C) 2008 Michael E. Houle,
+//               2012 Simon Wollwage
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. The source code and derived binary forms may be used only for
+//    non-commercial, non-profit research purposes.
+//
+// 2. Redistributions of source code must retain the above copyright
+//    notice, these conditions, and the following disclaimer.
+//
+// 3. Redistributions in binary form must reproduce the above copyright
+//    notice, these conditions, and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 4. The names of its contributors may not be used to endorse or promote
+//    products derived from this software without specific prior written
+//    permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Comments, bug fixes, etc welcome!
+// Contact e-mail address: meh@nii.ac.jp, meh@acm.org
+//                         mail.wollwage@gmail.com
+
 #include "RscClusterer.h"
 
 void RscClusterer::cluster_soft_rsc(const std::string& temp_directory_path, const std::string& cluster_directory_path)
@@ -6,14 +46,26 @@ void RscClusterer::cluster_soft_rsc(const std::string& temp_directory_path, cons
 	{
 		for (auto sender = 0; sender < Daemon::comm().size(); ++sender)
 			this->generate_patterns_for_sample(sample, transmission_mode, sender);
-
 		Daemon::comm().barrier();
 
 		for (auto sender = 0; sender < Daemon::comm().size(); ++sender)
 			this->generate_patterns_for_sample(sample, transmission_mode, boost::none);
-
 		Daemon::comm().barrier();
+
+		this->select_trim_patterns_for_sample(sample, Daemon::comm().rank() == 0 ? TransmissionSend : TransmissionReceive);
+		Daemon::comm().barrier();
+
+		this->select_final_patterns_for_sample(sample, Daemon::comm().rank() == 0 ? TransmissionSend : TransmissionReceive);
+		Daemon::comm().barrier();
+
+		this->generate_cluster_members_for_sample(sample, Daemon::comm().rank() == 0 ? TransmissionSend : TransmissionReceive);
+		Daemon::comm().barrier();
+
+		this->finalize_clusters_for_sample(sample);
+		Daemon::comm().barrier();		
 	}
+
+	this->finalize_clusters_and_build_graph();
 }
 
 void RscClusterer::generate_patterns_for_sample(const int sample_id, const TransmissionMode& transmission_mode, const boost::optional<unsigned int>& sender)
--- code/src/RscClusterer.h
+++ code/src/RscClusterer.h
@@ -1,3 +1,43 @@
+// GreedyRSC (Greedy Relevant Set Correlation) Clustering Tool
+//
+// Copyright (C) 2008 Michael E. Houle,
+//               2012 Simon Wollwage
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. The source code and derived binary forms may be used only for
+//    non-commercial, non-profit research purposes.
+//
+// 2. Redistributions of source code must retain the above copyright
+//    notice, these conditions, and the following disclaimer.
+//
+// 3. Redistributions in binary form must reproduce the above copyright
+//    notice, these conditions, and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 4. The names of its contributors may not be used to endorse or promote
+//    products derived from this software without specific prior written
+//    permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Comments, bug fixes, etc welcome!
+// Contact e-mail address: meh@nii.ac.jp, meh@acm.org
+//                         mail.wollwage@gmail.com
+
 #ifndef __RSC_CLUSTERER_H__
 #define __RSC_CLUSTERER_H__
 
